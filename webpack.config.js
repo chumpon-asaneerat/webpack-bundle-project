8432bc87b2de210407220b736c3d3c17e5cadb9c
chumpon-asaneerat <chumponsenate@gmail.com>
2019-08-02 17:50:11 +0700

    Update webpcack.config.js for enable sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -13,19 +13,22 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },
-    devtool: 'inline',
+    //devtool: 'inline',
+    devtool: "source-map",
     optimization: {
         minimize: true
     },
     module: {
         rules: [/*{
-            test: /\.riot$/i,
+            // riot tag (.riot, .tag) bundle setup
+            test: /\.(riot|tag)$/i,
             exclude: /node_modules/,
             use: [{
                 loader: '@riotjs/webpack-loader',
                 options: { hot: true }
             }]
-        },*/ {
+        }, */{
+            // js bundle setup
             test: /\.js$/i,
             exclude: /node_modules/,
             use: {
@@ -33,33 +36,38 @@ module.exports = {
                 options: { presets: ['@babel/preset-env'] }
             }
         }, {
+            // scss bundle setup
             test: /\.scss$/i,
             exclude: /node_modules/,
             use: [
                 {
                     // fallback to style-loader in development
                     loader: process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    options: { sourceMap: true }
                 },
                 {
                     loader: 'css-loader',
-                    options: { modules: false }
+                    options: { modules: false, sourceMap: true }
                 },
                 {
-                    loader: "sass-loader"
+                    loader: "sass-loader",
+                    options: { sourceMap: true }
                 }
             ]
         }, {
+            // css bundle setup
             test: /\.css$/i,
             exclude: /node_modules/,
             use: [
                 // style-loader
                 { 
-                    loader: 'style-loader'
+                    loader: 'style-loader',
+                    options: { sourceMap: true }
                 },
                 // css-loader
                 {
                     loader: 'css-loader',
-                    options: { modules: false }
+                    options: { modules: false, sourceMap: true }
                 }
             ]
         }, {
